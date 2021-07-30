03c3cb7acef224f064b1260329debae098260c82
TropicalTaco12 <aling0703@gmail.com>
2021-07-29 22:21:40 -0700

    Photos Webpage Slideshow

---
+++ assets/javascript.js
@@ -0,0 +1,55 @@
+/*var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+// Apply to all slideshows that you define with the markup wrote
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+  var index = 0, time = 1500;
+  slides[index].classList.add('active');  
+  
+  setInterval( () => {
+    slides[index].classList.remove('active');
+    
+    //Go over each slide incrementing the index
+    if()
+    index++;
+    
+    // If you go over all slides, restart the index to show the first slide and start again
+    if (index === slides.length) index = 0; 
+    
+    slides[index].classList.add('active');
+
+  }, time);
+}*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
